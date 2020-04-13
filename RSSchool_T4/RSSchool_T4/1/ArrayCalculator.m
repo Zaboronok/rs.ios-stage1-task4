5b911083816b1997e967fd4de0b9793ada114499
Yahor <egorzaboronok@gmail.com>
2020-04-13 23:39:15 +0300

    feat:done 1,2 task

--- RSSchool_T4/RSSchool_T4/1/ArrayCalculator.m
+++ RSSchool_T4/RSSchool_T4/1/ArrayCalculator.m
@@ -2,7 +2,33 @@
 
 @implementation ArrayCalculator
 + (NSInteger)maxProductOf:(NSInteger)numberOfItems itemsFromArray:(NSArray *)array {
-    // good luck
-    return -1;
+    
+    NSMutableArray *digits = [array mutableCopy];
+    NSInteger sum = 0;
+    NSInteger result = 1;
+    NSInteger flag = 0;
+    
+    NSArray *sorted = [digits sortedArrayUsingComparator:^(id a, id b) {
+        if ([a intValue] < [b intValue]) {
+            return NSOrderedDescending;
+        } else if ([b intValue] < [a intValue]) {
+            return NSOrderedAscending;
+        } else {
+            return NSOrderedSame;
+        }
+    }];
+    
+    for (int i = 0; i < [sorted count]; i++) {
+        if(flag < numberOfItems){
+            if ([sorted[i] isKindOfClass:[NSString class]]) {
+                continue;
+            }
+            result = result * [sorted[i] integerValue];
+            sum = result;
+            flag += 1;
+        }
+    }
+    
+    return sum;
 }
 @end
--- RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
+++ RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
@@ -2,7 +2,38 @@
 
 @implementation SquareDecomposer
 - (NSArray <NSNumber*>*)decomposeNumber:(NSNumber*)number {
-    // good luck
-    return @[@0];
+    
+    NSNumber *squared = [NSNumber numberWithInt:([number intValue] * [number intValue])];
+    
+    NSMutableArray *decomposeArray = [self decomposer:number squared:squared];
+    
+    if(decomposeArray == nil){
+        return nil;
+    }else{
+        [decomposeArray removeLastObject];
+        return decomposeArray;
+    }
+}
+
+-(NSMutableArray *)decomposer:(NSNumber *) num squared:(NSNumber *) squared {
+    if ([squared longLongValue] == 0) {
+        return [NSMutableArray arrayWithObject:num];
+    }
+    
+    for(long long i = [num longLongValue] - 1; i > 0; i--) {
+        long long iSquare = i * i;
+        
+        if(([squared longLongValue] - iSquare) >= 0){
+            NSNumber *nextNum = [NSNumber numberWithLongLong:i];
+            NSNumber *nextSquared = [NSNumber numberWithLongLong:([squared longLongValue] - iSquare)];
+            NSMutableArray *arr = [self decomposer:nextNum squared:nextSquared];
+            if (arr != nil){
+                [arr addObject:num];
+                return arr;
+            }
+        }
+    }
+    
+    return nil;
 }
 @end
